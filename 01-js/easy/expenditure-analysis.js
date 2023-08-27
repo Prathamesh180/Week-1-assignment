5e7d615693b5e42d4ac1ac9a5c0fbaa028ca438e
Prathamesh Jayale <85394583+Prathamesh180@users.noreply.github.com>
2023-08-27 18:14:57 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,6 +8,7 @@
   - `npm run test-expenditure-analysis`
 */
 
+Part 1: Used transactions array of objects to create res object of categories and spendEstimates value.
 function calculateTotalSpentByCategory(transactions) {
   let res = {};
   
@@ -19,7 +20,8 @@ function calculateTotalSpentByCategory(transactions) {
       res[t.category] = t.price;
     }
   }
-  
+
+  Part 2: Used res object to create array of objects using Objects.keys() method.
   let ans = [];
   let keys = Object.keys(res);
   for(let i = 0; i < keys.length; i++){
